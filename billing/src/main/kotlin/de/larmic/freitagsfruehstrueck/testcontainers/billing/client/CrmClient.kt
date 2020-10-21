43f2def8a875b9d05b1c06eb4a19e46b2212fddc
Lars Michaelis <mail@larmic.de>
2020-10-21 15:32:57 +0200

    use khttp and kotlinx.serialization

--- billing/src/main/kotlin/de/larmic/freitagsfruehstrueck/testcontainers/billing/client/CrmClient.kt
+++ billing/src/main/kotlin/de/larmic/freitagsfruehstrueck/testcontainers/billing/client/CrmClient.kt
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import khttp.get
 import khttp.responses.Response
-import org.json.JSONObject
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 @Service
 class CrmClient(@Value("\${crm.rest.url}") private var crmUrl: String) {
 
-    fun readCustomer(customerId: String) = get("$crmUrl/api/customer/$customerId").toCustomer()
+    fun readCustomer(customerId: String) = get("$crmUrl/api/customer/$customerId").decodeToCustomer()
 
-    private fun Response.toCustomer() = this.jsonObject.toCustomer()
-    private fun JSONObject.toCustomer() = Customer(id = this["id"] as String, name = this["name"] as String, iban = this["iban"] as String)
+    private fun Response.decodeToCustomer() = Json.decodeFromString<Customer>(this.text)
 }
 
+@Serializable
 class Customer(val id: String,
                val name: String,
                val iban: String)

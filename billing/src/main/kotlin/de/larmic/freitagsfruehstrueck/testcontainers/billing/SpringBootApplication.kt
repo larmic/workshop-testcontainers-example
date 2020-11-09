eac925114b2b02b5a719fca1b40c4870f050d795
Lars Michaelis <mail@larmic.de>
2020-11-09 08:11:22 +0100

    bump testcontainers to 1.15.0

--- billing/src/main/kotlin/de/larmic/freitagsfruehstrueck/testcontainers/billing/SpringBootApplication.kt
+++ billing/src/main/kotlin/de/larmic/freitagsfruehstrueck/testcontainers/billing/SpringBootApplication.kt
@@ -2,8 +2,6 @@ package de.larmic.freitagsfruehstrueck.testcontainers.billing
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 open class SpringBootElasticsearchOverHttpApplication
